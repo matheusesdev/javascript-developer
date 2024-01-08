1be1851d9b3471099c958e2a60aafeae7bcba78d
Matheus Santos <matheuses18@icloud.com>
2024-01-08 16:06:54 -0300

    add listadeExercicio1.js and listadeExercicio2.js

---
+++ 2- Condicionais/listadeExercicio1.js
@@ -0,0 +1,23 @@
+/* Faça um algoritmo que dado as 3 notas tiradas por um aluno em um semestre da faculdade, calcule e imprima a sua média e a sua classificação conforme a tabela abaixo.
+
+Média = (nota1 + nota2 + nota3) / 3
+
+Classificação:
+- Média menor que 5, reprovado;
+- Média entre 5 e 7, recuperação;
+- Média acima de 7, passou de semestre.
+*/
+
+const nota1 = 1;
+const nota2 = 1;
+const nota3 = 1;
+const media = (nota1 + nota2 + nota3) / 3;
+console.log(media);
+
+if (media < 5) {
+  console.log("Você foi reprovado");
+} else if (media >= 5 && media <= 7){
+    console.log("Você está de recuperação");
+} else{
+    console.log("Você passou de semestre");
+}
