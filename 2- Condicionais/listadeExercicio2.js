1192edebd145e3cccab08ddfa89f9d58f38f1eca
Matheus Santos <matheuses18@icloud.com>
2024-01-08 17:45:02 -0300

    update listadeExercicio2.js

--- 2- Condicionais/listadeExercicio2.js
+++ 2- Condicionais/listadeExercicio2.js
@@ -13,18 +13,19 @@ IMC em adultos Condição:
 - Acima de 40 = Obesidade Grave;
 */
 
-const peso = 65;
-const altura = 1.70;
+const peso = 80;
+const altura = 1.75;
 const imc = peso / Math.pow(altura, 2); //Potência de altura ao quadrado.
+console.log(imc.toFixed(2));
 
-if (imc < 18.5){
-    console.log("Abaixo do peso.")
-} else if (imc >= 18.5 && imc <= 25){
-    console.log ("Peso normal");
-} else if (imc >= 25 && imc <= 30){
-    console.log("Acima do peso");
-} else if (imc >= 30 && imc <= 40){
-    console.log("Acima do peso");
+if (imc < 18.5) {
+  console.log("Abaixo do peso.");
+} else if (imc >= 18.5 && imc < 25) {
+  console.log("Peso normal");
+} else if (imc >= 25 && imc < 30) {
+  console.log("Acima do peso");
+} else if (imc >= 30 && imc < 40) {
+  console.log("Acima do peso");
 } else {
-    console.log ("Obesidade grave")
-} 
+  console.log("Obesidade grave");
+}
