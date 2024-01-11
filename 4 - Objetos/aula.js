10ef59b60b9e708ab1d78455784654b6c0563b61
Matheus Santos <matheuses18@icloud.com>
2024-01-11 11:09:34 -0300

    update aula.js

--- 4 - Objetos/aula.js
+++ 4 - Objetos/aula.js
@@ -1,13 +1,20 @@
-const pessoa = {
-    nome: 'Matheus',
-    idade: 26,
+class Pessoa { // Classe é a definição do que deve ser o  objeto
+    nome;
+    idade;
+    anoDeNascimento;
 
-    descrever: function () {
-        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`)
+    constructor(nome, idade){ //Constructor é o que acontece quando algo é instanciado.
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
+     descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade} anos, e meu ano de nascimento é ${this.anoDeNascimento}.`)
+    }
 }
-}
+const matheus = new Pessoa('Maria', 27); // Instância é uma ocorrência daquele objeto.
+const joao = new Pessoa('Clebson', 22);
 
 
-const atributo = `nome`;
 
-console.log(pessoa[atributo]);
+console.log(matheus)
