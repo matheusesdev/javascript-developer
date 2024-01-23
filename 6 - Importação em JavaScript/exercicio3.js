300c80e4c4804e5fe4961e6a0683dacfaf211e14
matheusesdev <matheuses18@icloud.com>
2024-01-23 09:31:50 -0300

    Update exercicio3.js

--- 6 - Importação em JavaScript/exercicio3.js
+++ 6 - Importação em JavaScript/exercicio3.js
@@ -6,20 +6,20 @@ Saída: Maior número par: 10 // Menor número ímpar: 1 */
 
 const { gets, print } = require("./funcoesauxiliaresex3.js");
 
-const n = gets();
+const n = gets(); //Aqui eu recebo o número de valores
+let maiorNumeroPar = null; //Aqui eu tenho o maior número par, que ainda não sei qual o valor
+let menorNumeroImpar = null; //Aqui eu tenho o menor número ímpar, que eu ainda não sei o valor.
 
-let maiorNumeroPar = 0;
-let menorNumeroImpar = 0;
 for (let i = 0; i < n; i++) {
+  //Aqui eu vou percorrer o trecho de códigos N vezes, por que é a quantidade de números que vai ser informada. E cada vez vai pegar um número novo para avaliar.
   const numero = gets();
+
   if (numero % 2 === 0) {
-    if (numero > maiorNumeroPar) {
+    if (maiorNumeroPar === null || numero > maiorNumeroPar) {
       maiorNumeroPar = numero;
     }
   } else {
-    if (menorNumeroImpar === 0) {
-      menorNumeroImpar = numero;
-    } else if (numero < menorNumeroImpar) {
+    if (menorNumeroImpar === null || numero < menorNumeroImpar) {
       menorNumeroImpar = numero;
     }
   }
