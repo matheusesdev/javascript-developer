7fe06cd6c26c5c8ae2464ef1293024167665611a
matheusesdev <matheuses18@icloud.com>
2024-01-23 09:08:27 -0300

    atividade funcoes auxiliares

--- 6 - Importação em JavaScript/exercicio3.js
+++ 6 - Importação em JavaScript/exercicio3.js
@@ -9,13 +9,21 @@ const { gets, print } = require("./funcoesauxiliaresex3.js");
 const n = gets();
 
 let maiorNumeroPar = 0;
-
+let menorNumeroImpar = 0;
 for (let i = 0; i < n; i++) {
   const numero = gets();
   if (numero % 2 === 0) {
     if (numero > maiorNumeroPar) {
       maiorNumeroPar = numero;
     }
+  } else {
+    if (menorNumeroImpar === 0) {
+      menorNumeroImpar = numero;
+    } else if (numero < menorNumeroImpar) {
+      menorNumeroImpar = numero;
+    }
   }
 }
-print(maiorNumeroPar);
+
+print("Maior número par é: " + maiorNumeroPar);
+print("O menor número ímpar é: " + menorNumeroImpar);
