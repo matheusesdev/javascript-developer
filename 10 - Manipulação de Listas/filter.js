d9d4bcc75988299094c526c997ba310a917f2c5a
matheusesdev <matheuses18@icloud.com>
2024-02-21 20:35:55 -0300

    Revert "Função e manipulação de listas com JavaScript, Promisses e assincronismo."
    This reverts commit f66a460c93fb4a5b14d6b31bada38872798bb464.

--- 10 - Manipulação de Listas/filter.js
+++
@@ -1,8 +0,0 @@
-const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const listaDeNumeroPar = lista.filter((element, i, listRef) => {
-  return element % 2 === 0;
-});
-
-console.log(lista);
-console.log(listaDeNumeroPar);
--- 10 - Manipulação de Listas/join.js
+++
@@ -1,6 +0,0 @@
-const lista = [{ nome: "Matheus" }, { nome: "Maria" }, { nome: "João" }];
-
-
-
-
-console.log(lista.map((e)=> e.nome).join(':'))
